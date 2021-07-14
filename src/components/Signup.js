c786ad1241f6179fbeaa165f7c4d6c789439eea6
ariful-1122 <greenheart970@gmail.com>
2021-07-14 18:51:38 +0600

    loading and error state complete

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
-import { Button, Card, Cart, Form } from "react-bootstrap";
+import { Alert, Button, Card, Cart, Form } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
 
 const Signup = () => {
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const { signup } = useAuth();
   const emailRef = useRef();
@@ -15,10 +16,17 @@ const Signup = () => {
     const confirmEnteredPassword = passwordConfirmRef.current.value;
 
     if (enteredPassword !== confirmEnteredPassword) {
-      return setError("password don't");
+      return setError("password don't match");
     }
 
-    signup(emailRef.current.value, passwordRef.current.value);
+    try {
+      setError("");
+      setLoading(true);
+      signup(emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
   }
 
   return (
@@ -26,7 +34,8 @@ const Signup = () => {
       <Card className="col-md-6 mx-auto my-4">
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger"> {error} </Alert>}
+          <Form onSubmit={submitHandler}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control ref={emailRef} type="email" required />
@@ -41,7 +50,7 @@ const Signup = () => {
               <Form.Label>Email</Form.Label>
               <Form.Control ref={passwordConfirmRef} type="password" required />
             </Form.Group>
-            <Button className="mt-3" type="submit">
+            <Button disabled={loading} className="mt-3" type="submit">
               Sign up
             </Button>
           </Form>
